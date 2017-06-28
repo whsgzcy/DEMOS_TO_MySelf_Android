997e7c8e2d30c69b433b62cc4c180bacf39f087f
whsgzcy <whsgzcy@foxmail.com>
2017-06-28 09:04:48 +0800

    sweet alert dialog

---
+++ app/src/main/java/com/example/super_yu/myexample/dialog/Dialog2Activity.java
@@ -0,0 +1,27 @@
+package com.example.super_yu.myexample.dialog;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.super_yu.myexample.R;
+
+public class Dialog2Activity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog2);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/super_yu/myexample/remedia/Remarker2Activity.java
@@ -0,0 +1,33 @@
+package com.example.super_yu.myexample.remedia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.super_yu.myexample.R;
+
+public class Remarker2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remarker2);
+
+
+        Button shakeBtn = (Button)findViewById(R.id.shake);
+
+
+
+
+
+
+
+
+        Button ringBtn = (Button)findViewById(R.id.ring);
+
+
+
+
+
+    }
+}
