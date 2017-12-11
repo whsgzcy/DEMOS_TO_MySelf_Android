3064ccd0c93592785f6bf756b100cb67ffbeec07
whsgzcy <whsgzcy@foxmail.com>
2017-12-11 13:19:49 +0800

    内存泄露追踪，

--- app/src/main/java/com/example/super_yu/myexample/MyExample.java
+++ app/src/main/java/com/example/super_yu/myexample/MyExample.java
@@ -20,6 +20,14 @@ public class MyExample extends Application {
     @Override
     public void onCreate() {
 
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code...
+
         // 应用程序入口处调用，避免手机内存过小，杀死后台进程后通过历史intent进入Activity造成SpeechUtility对象为null
         // 如在Application中调用初始化，需要在Mainifest中注册该Applicaiton
         // 注意：此接口在非主进程调用会返回null对象，如需在非主进程使用语音功能，请增加参数：SpeechConstant.FORCE_LOGIN+"=true"
