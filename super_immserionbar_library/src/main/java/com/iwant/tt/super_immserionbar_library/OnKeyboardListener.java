f36edafba11375abeb5d06d892c7354a9e344b82
whsgzcy <whsgzcy@foxmail.com>
2018-02-07 17:37:26 +0800

    Activity bar 样式

---
+++ super_immserionbar_library/src/main/java/com/iwant/tt/super_immserionbar_library/OnKeyboardListener.java
@@ -0,0 +1,14 @@
+package com.iwant.tt.super_immserionbar_library;
+
+/**
+ * 软键盘监听
+ */
+public interface OnKeyboardListener {
+    /**
+     * On keyboard change.
+     *
+     * @param isPopup        the is popup  是否弹出
+     * @param keyboardHeight the keyboard height  软键盘高度
+     */
+    void onKeyboardChange(boolean isPopup, int keyboardHeight);
+}
