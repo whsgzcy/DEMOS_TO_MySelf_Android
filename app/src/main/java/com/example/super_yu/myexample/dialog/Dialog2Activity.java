6df766f24199aeaa548241d1f6574869cb7803fc
whsgzcy <whsgzcy@foxmail.com>
2018-03-13 11:47:12 +0800

    修改 dialog 样式

--- app/src/main/java/com/example/super_yu/myexample/dialog/Dialog2Activity.java
+++ app/src/main/java/com/example/super_yu/myexample/dialog/Dialog2Activity.java
@@ -6,6 +6,7 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -191,6 +192,7 @@ public class Dialog2Activity extends AppCompatActivity {
                                 null);
                 mMaterialDialog.setCanceledOnTouchOutside(true);
                 mMaterialDialog.setView(view).show();
+
             }
             break;
             case R.id.button_set_background: {
